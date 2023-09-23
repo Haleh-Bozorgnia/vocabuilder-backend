91af2910fa7d55385982c1df85d73100ad1ad998
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-09-23 10:22:56 -0700

    edit

--- vocabproject/settings.py
+++ vocabproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "cheeses.apps.CheesesConfig",
+    "vocabs.apps.VocabsConfig",
     "rest_framework",
     "corsheaders",
 ]
--- vocabs/views.py
+++ vocabs/views.py
@@ -6,5 +6,4 @@ class VocabViewSet(viewsets.ModelViewSet):
     queryset = Vocab.objects.all()
     # The serializer_class attribute is used to control which serializer class should be used for serializing and deserializing queryset and model instances.
     serializer_class = VocabSerializer
-    # Set permission_classes to allow unrestricted access to the API.
     permission_classes = [permissions.AllowAny]
